4a4b746e902073e359a0a11466f84d5dea253c69
Simon Schiller <simon.schiller1998@gmail.com>
2021-01-26 15:13:05 +0100

    Fix version config mismatch

--- plugin/src/main/kotlin/io/github/simonschiller/permissioncheck/config/TaskConfiguratorFactory.kt
+++ plugin/src/main/kotlin/io/github/simonschiller/permissioncheck/config/TaskConfiguratorFactory.kt
@@ -9,7 +9,7 @@ internal object TaskConfiguratorFactory {
     fun getTaskConfigurator(): TaskConfigurator {
         val version = VersionNumber.parse(Version.ANDROID_GRADLE_PLUGIN_VERSION)
         return when {
-            version <= VersionNumber.parse("4.0.1") -> TaskConfiguratorV1()
+            version <= VersionNumber.parse("4.0.2") -> TaskConfiguratorV1()
             version <= VersionNumber.parse("4.1.2") -> TaskConfiguratorV2()
             else -> TaskConfiguratorV3()
         }
