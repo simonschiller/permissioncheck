84a4ed065630d7fb5bf6a9caeecc622939513eed
Simon Schiller <simon.schiller1998@gmail.com>
2021-06-02 19:26:06 +0200

    Prepare for AGP 7.0.0

---
+++ plugin-configurator-v4/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    compileOnly(gradleKotlinDsl())
+    compileOnly("com.android.tools.build:gradle:7.0.0-beta03")
+
+    api(project(":plugin-core"))
+}
