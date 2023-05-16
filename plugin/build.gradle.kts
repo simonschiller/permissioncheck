62d7dabf5d123bce9db824457eb716227166c707
Josep Rubio <jose.rubiomorente@telefonica.com>
2023-05-16 17:39:09 +0200

    ANDROID-11430 Update maven info

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
     kotlin("jvm")
     `java-gradle-plugin`
     `maven-publish`
-    id("com.gradle.plugin-publish") version "0.14.0"
+    id("com.gradle.plugin-publish") version "1.2.0"
 }
 
-group = "io.github.simonschiller"
-version = "1.7.0" // Also update the version in the README
+group = "com.telefonica"
+version = "1.0.0" // Also update the version in the README
 
 val uber: Configuration by configurations.creating
 
@@ -41,27 +41,15 @@ tasks.withType<Test>().configureEach {
 
 gradlePlugin {
     plugins {
-        create("permissioncheck") {
-            id = "io.github.simonschiller.permissioncheck"
+        create("manifestcheck") {
+            id = "com.telefonica.manifestcheck"
+            displayName = "ManifestCheck"
+            description = "ManifestCheck is a Gradle plugin that helps you catch Android permission/feature regressions automatically."
             implementationClass = "io.github.simonschiller.permissioncheck.PermissionCheckPlugin"
         }
     }
 }
-
 pluginBundle {
-    website = "https://github.com/simonschiller/permissioncheck"
-    vcsUrl = "https://github.com/simonschiller/permissioncheck"
-    description = "PermissionCheck is a Gradle plugin that helps you catch Android permission regressions automatically."
-    tags = listOf("android", "permissions")
-
-    mavenCoordinates {
-        groupId = project.group.toString()
-        artifactId = "permissioncheck"
-    }
-
-    (plugins) {
-        "permissioncheck" {
-            displayName = "PermissionCheck"
-        }
-    }
+    website = "https://https://github.com/Telefonica/android-permissioncheck"
+    vcsUrl = "https://https://github.com/Telefonica/android-permissioncheck"
 }
